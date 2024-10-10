204f0304adee91efc62c8c62aca280015bafce77
Vorobiy <eugenevorob@gmail.com>
2024-10-10 12:24:23 -0400

    V1.0.0 FULLY WORKING DIGIT RECOGNITION

--- main.py
+++ main.py
@@ -1,48 +1,66 @@
 import os
 import cv2
 import numpy as np
-import matplotlib as plt
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
+print("Welcome to the NeuralNine (c) Handwritten Digits Recognition v0.1")
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# Decide if to load an existing model or to train a new one
+train_new_model = True
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
-'''
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(10, activation = 'softmax'))
+if train_new_model:
+    # Loading the MNIST data set with samples and splitting it
+    mnist = tf.keras.datasets.mnist
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # Normalizing the data (making length = 1)
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 
-model.fit(x_train, y_train, epochs=3)
+    # Create a neural network model
+    # Add one flattened input layer for the pixels
+    # Add two dense hidden layers
+    # Add one dense output layer for the 10 digits
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-model.save('handwritten.keras')
-'''
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model = tf.keras.models.load_model('handwritten.keras')
+    # Compiling and optimizing model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+    # Training the model
+    model.fit(X_train, y_train, epochs=10)
+
+    # Evaluating the model
+    val_loss, val_acc = model.evaluate(X_test, y_test)
+    print(val_loss)
+    print(val_acc)
+
+    # Saving the model
+    model.save('handwritten_digits.keras')
+else:
+    # Load the model
+    model = tf.keras.models.load_model('handwritten_digits.model')
+
+# Load custom images and predict them
 image_number = 1
-while os.path.isfile(f"digits/digit{image_number}.png"):
+while os.path.isfile('digits/digit{}.png'.format(image_number)):
     try:
-        img = cv2.imread(f"digit/digits{image_number}.png")[:,:,0]
+        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
         img = np.invert(np.array([img]))
         prediction = model.predict(img)
-        print("This digit is {np.argmax(prediction)}")
+        print("The number is probably a {}".format(np.argmax(prediction)))
         plt.imshow(img[0], cmap=plt.cm.binary)
         plt.show()
-    except:
-        print("Error!")
-    finally:
         image_number += 1
-
-
-
-
-
-
-
+    except:
+        print("Error reading image! Proceeding with next image...")
+        image_number += 1
